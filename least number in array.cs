4c9f15510309846ba6a118cf020cda7854d55cbb
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2023-11-08 22:34:03 +0200

    Create least number in array.cs
    Given an array of integers your solution should find the smallest integer.

---
+++ least number in array.cs
@@ -0,0 +1,17 @@
+public class Kata
+{
+    public static int FindSmallestInt(int[] args) 
+    {
+            int min=args[0];
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (min > args[i])
+                {
+                    min = args[i];
+                }
+            }
+            return min;
+    
+
+    }
+}
