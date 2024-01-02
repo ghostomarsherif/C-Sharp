5880819f0be60bb55a94d5af3f3adc8acfe7dbd5
Omar Sherif Mohammed <125500957+ghostomarsherif@users.noreply.github.com>
2024-01-02 20:04:32 +0200

    Create Number_Pyramid.cs
    Write a C# program which produces the following output : (hint: use two
    nested FOR loops)
    1
    22
    333
    4444
    55555

---
+++ Console Programming/Number_Pyramid.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+
+namespace omar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
